1eb2211aca1b7f202e6f43f2b5c309fac401da70
Henrry Alvarado <princedark393@gmail.com>
2020-12-15 19:09:12 -0500

    Creo aplicacion web con visual estudio utilizando MVC

---
+++ Aplication.Web/Aplication.Web/Models/MyData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplication.Web.Models
+{
+    public class MyData
+    {
+        public string Name { get; set; }
+        public string LastName{ get; set; }
+        public string Institute { get; set; }
+        public string Level { get; set; }
+        public string Activities { get; set; }
+        public string Conctact { get; set; }
+        public DateTime Publicacion { get; set; }
+
+    }
+}
---
+++ Aplication.Web/Aplication.Web/Service/MyDataService.cs
@@ -0,0 +1,26 @@
+﻿using Aplication.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplication.Web.Service
+{
+    public class MyDataService
+    {
+        public MyData ObtenerDatos()
+        {
+            return new MyData()
+            {
+                Name = "Henrry",
+                LastName = "Alvarado",
+                Institute = "Benito Juarez",
+                Level = "Tercer Nivel",
+                Activities = "Estudiante - Desarrollador",
+                Conctact = "0991345673",
+                Publicacion = new DateTime(2020, 12, 16, 5, 30, 15)
+            };
+
+        }
+    }
+}
